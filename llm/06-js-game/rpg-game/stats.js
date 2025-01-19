dc7abe3b732458b764f18782989da9272b44a52e
dazzayah <dazzayah90032@gmail.com>
2025-01-20 00:10:13 +0100

    portfolio startup project save, js rpg game started

---
+++ llm/06-js-game/rpg-game/stats.js
@@ -0,0 +1,13 @@
+const strength = sessionStorage.getItem('strength');
+const intellect = sessionStorage.getItem('intellect');
+const spirit = sessionStorage.getItem('spirit');
+const agility = sessionStorage.getItem('agility');
+const stamina = sessionStorage.getItem('stamina');
+
+console.log('Fuerza:', strength);
+console.log('Intelecto:', intellect);
+console.log('Espíritu:', spirit);
+console.log('Agilidad:', agility);
+console.log('Aguante:', stamina);
+
+
