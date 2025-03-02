9234bc1381cab24fd21e5f5ccfbfa7f6148c2a6d
Iliá Briansó <dazzayah@Ilias-MacBook-Air.local>
2025-03-02 21:23:43 +0100

    xml and json validation progress

--- gbd/05-mysql-subqueries/subqueries-ilia.sql
+++ gbd/05-mysql-subqueries/subqueries-ilia.sql
@@ -1,85 +1,31 @@
-USE ILIA_PT0;
+USE sanitat;
+-- 1
+SELECT HOSPITAL_COD, NOM, TELEFON
+FROM HOSPITAL;
 
--- 1. Mostrar el nom dels departaments ubicats a la mateixa ciutat que el departament d’INFORMÀTICA
-SELECT nom_dept, ciutat
-FROM departaments
-WHERE ciutat = (SELECT ciutat
-                FROM departaments
-                WHERE nom_dept = 'INFORMATICA');
+-- 2
+SELECT HOSPITAL_COD, NOM, TELEFON
+FROM HOSPITAL WHERE NOM LIKE '_a%';
 
--- 2. Obtenir el codi i el nom dels empleats que guanyen més diners que l’empleat ‘5’
-SELECT codi_emp, nom_emp
-FROM empleats
-WHERE sou > (SELECT sou
-             FROM empleats
-             WHERE codi_emp = 5);
+-- 3
+SELECT HOSPITAL_COD, SALA_COD, EMPLEAT_NO, COGNOM
+FROM PLANTILLA;
 
--- 3. Obtenir els companys de departament de l’empleat ‘5’
-SELECT codi_emp, nom_emp
-FROM empleats
-WHERE codi_dept = (SELECT codi_dept
-                   FROM empleats
-                   WHERE codi_emp = 5);
+-- 4
+SELECT HOSPITAL_COD, SALA_COD, EMPLEAT_NO, COGNOM
+FROM PLANTILLA WHERE TORN != 'N';
 
--- 4. Obtenir el codi i el nom de tots els empleats assignats al projecte 'Daisy'
-SELECT codi_emp, nom_emp
-FROM empleats
-WHERE codi_proj = (SELECT codi_proj
-                   FROM projectes
-                   WHERE nom_proj = 'Daisy');
+-- 5
+SELECT * FROM MALALT
+WHERE DATA_NAIX LIKE '1960-__-__';
 
--- 5. Mostrar quants empleats tenen assignat el projecte 'Daisy'
-SELECT COUNT(*)
-FROM empleats
-WHERE codi_proj = (SELECT codi_proj
-                   FROM projectes
-                   WHERE nom_proj = 'Daisy');
+-- 6
+SELECT * FROM MALALT
+WHERE DATA_NAIX >= '1960-01-01';
 
--- 6. Mostrar els departaments que tenen assignat un empleat en el projecte ‘CLAM’
-SELECT codi_dept, nom_dept
-FROM departaments
-WHERE codi_dept IN (SELECT codi_dept
-                    FROM empleats
-                    WHERE codi_proj = (SELECT codi_proj
-                                       FROM projectes
-                                       WHERE nom_proj = 'CLAM'));
+-- 7
+SELECT COUNT(*), SEXE
+FROM MALALT
+GROUP BY SEXE;
 
--- 7. Mostrar el codi i el nom dels empleats que treballen en el departament de 'COMPTABILITAT' o d’ 'ADMINISTRACIO'
-SELECT codi_emp, nom_emp
-FROM empleats
-WHERE codi_dept = (SELECT codi_dept
-                   FROM departaments
-                   WHERE nom_dept = 'COMPTABILITAT')
-   OR codi_dept = (SELECT codi_dept
-                   FROM departaments
-                   WHERE nom_dept = 'ADMINISTRACIO');
-
--- 8. Mostrar quants empleats que treballen a ‘Madrid’ o a ‘Barcelona’ tenen un sou més gran que la mitjana de sous del departament de ‘RECURSOS HUMANS’
-SELECT COUNT(*) AS Empleats
-FROM empleats
-WHERE codi_dept IN (SELECT codi_dept
-                    FROM departaments
-                    WHERE ciutat = 'Madrid')
-   OR codi_dept IN (SELECT codi_dept
-                    FROM departaments
-                    WHERE ciutat = 'Barcelona')
-    AND sou > (SELECT AVG(sou)
-               FROM empleats
-               WHERE codi_dept = (SELECT codi_dept
-                                  FROM departaments
-                                  WHERE nom_dept = 'RECURSOS HUMANS'));
-
--- 9. Consultar els empleats que treballen en un projecte amb un pressupost més gran que 70.000
-SELECT codi_emp, nom_emp
-FROM empleats
-WHERE codi_proj IN (SELECT codi_proj
-                    FROM projectes
-                    WHERE pressupost > 70000) ORDER BY codi_emp;
-
--- 10. Obtenir el nom dels empleats que tenen assignat un projecte amb el pressupost més petit
-SELECT nom_emp
-FROM empleats
-WHERE codi_proj IN (SELECT codi_proj
-                    FROM projectes
-                    WHERE pressupost = (SELECT min(pressupost)
-                                        FROM projectes));
+-- 8
